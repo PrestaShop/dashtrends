cd4aa2f6eff4cfb0e24e206b1384a60392972acf
Jerome Nadaud <jerome.nadaud@gmail.com>
2015-01-20 12:06:54 +0100

    [-] MO : dashtrends - FixBug #PSCSX-4386 - Force cast on y axis data

--- views/js/dashtrends.js
+++ views/js/dashtrends.js
@@ -7,12 +7,12 @@ function line_chart_trends(widget_name, chart_details)
 		$('#dash_trends_chart1').hide();
 	else
 		$('#dash_trends_chart1').show();
-	nv.addGraph(function() {  
+	nv.addGraph(function() {
 		var chart = nv.models.lineChart()
 			.useInteractiveGuideline(true)
 			.x(function(d) { return (d !== undefined ? d[0] : 0); })
-			.y(function(d) { return (d !== undefined ? d[1] : 0); });
-	
+			.y(function(d) { return (d !== undefined ? parseInt(d[1]) : 0); });
+
 		chart.xAxis.tickFormat(function(d) {
 			date = new Date(d*1000);
 			return date.format(chart_details['date_format']);
@@ -24,7 +24,7 @@ function line_chart_trends(widget_name, chart_details)
 			{
 				if (value.id == 'sales')
 					$('#dashtrends_toolbar dl:first').css('background-color', chart_details.data[index].color).addClass('active');
-				first_data.push(chart_details.data[index]); 
+				first_data.push(chart_details.data[index]);
 			}
 		});
 
@@ -38,7 +38,7 @@ function line_chart_trends(widget_name, chart_details)
 		d3.select('#dash_trends_chart1 svg')
 			.datum(first_data)
 			.call(chart);
-		nv.utils.windowResize(chart.update);
+		nv.utils.windowResize(function() {chart.update});
 
 		return chart;
 	});
@@ -56,7 +56,8 @@ function selectDashtrendsChart(element, type)
 				$(element).siblings().css('background-color', 'none').removeClass('active');
 				$(element).css('background-color', dashtrends_data[index].color).addClass('active');
 			}
-			current_charts.push(dashtrends_data[index]); 
+
+			current_charts.push(dashtrends_data[index]);
 			value.disabled = false;
 		}
 	});
@@ -76,6 +77,8 @@ function selectDashtrendsChart(element, type)
 	d3.select('#dash_trends_chart1 svg')
 		.datum(current_charts)
 		.call(dashtrends_chart);
+
+	nv.utils.windowResize(function() {dashtrends_chart.update});
 }
 
 $(document).ready(function(){
