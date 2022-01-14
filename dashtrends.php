19e099eaeb1c0aa7b0a95286b63801461f004bc9
okom3pom <contact@okom3pom.com>
2022-01-14 10:45:44 +0100

    Update dashtrends.php

--- dashtrends.php
+++ dashtrends.php
@@ -48,7 +48,7 @@ class dashtrends extends Module
     public function __construct()
     {
         $this->name = 'dashtrends';
-        $this->tab = 'dashboard';
+        $this->tab = 'administration';
         $this->version = '2.0.3';
         $this->author = 'PrestaShop';
 
